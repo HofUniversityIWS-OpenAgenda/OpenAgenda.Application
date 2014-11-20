83036685f2b74884ecc91845407669e2c7b5ab5b
Oliver Hader <oliver@typo3.org>
2014-11-20 15:19:43 +0100

    [TASK] Define Login.index as default action route

--- Classes/OpenAgenda/Application/Controller/LoginController.php
+++ Classes/OpenAgenda/Application/Controller/LoginController.php
@@ -10,13 +10,13 @@ use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Mvc\Controller\ActionController;
 use OpenAgenda\Application\Domain\Model\Account;
 
-class StandardController extends ActionController {
+class LoginController extends ActionController {
 
 	/**
 	 * @return void
 	 */
 	public function indexAction() {
-		$this->redirect('index', 'Login');
+
 	}
 
 }
