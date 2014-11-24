7d3a6d6b09ce0d993d4c9ab997f8cdc420636bba
Andreas Steiger <andreas.steiger@hof-university.de>
2014-11-24 13:19:05 +0100

    [TASK] Create Security\PermissionService

---
+++ Classes/OpenAgenda/Application/Security/PermissionService.php
@@ -0,0 +1,33 @@
+<?php
+namespace OpenAgenda\Application\Security;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "OpenAgenda.Application".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+
+class PermissionService {
+
+/**
+* @var
+*/
+	protected $securityContext;
+
+	/**
+	 * @return void
+	 */
+	public function isControllerAllowed() {
+		
+	}
+
+	/**
+	 * @return void
+	 */
+	public function isControllerActionAllowed() {
+
+	}
+
+}
