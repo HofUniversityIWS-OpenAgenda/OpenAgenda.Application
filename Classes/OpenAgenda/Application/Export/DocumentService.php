2dc2f7c51f4a94a7257f70ea60877637e2041dd4
Andreas Steiger <andreas.steiger@hof-university.de>
2014-11-24 13:53:56 +0100

    [TASK] Create Export\DocumentService

---
+++ Classes/OpenAgenda/Application/Export/DocumentService.php
@@ -0,0 +1,28 @@
+<?php
+namespace OpenAgenda\Application\Export;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "OpenAgenda.Application".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+
+class DocumentService {
+
+	/**
+	 * @return void
+	 */
+	public function exportAgenda() {
+
+	}
+
+	/**
+	 * @return void
+	 */
+	public function exportProtocol() {
+
+	}
+
+}
