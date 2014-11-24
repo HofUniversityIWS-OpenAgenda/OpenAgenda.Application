98e4c342c2ab733b62dfed554e12313350a1f4dc
Andreas Steiger <andreas.steiger@hof-university.de>
2014-11-24 12:25:05 +0100

    [TASK] Update Models

--- Classes/OpenAgenda/Application/Domain/Model/History.php
+++ Classes/OpenAgenda/Application/Domain/Model/History.php
@@ -15,24 +15,110 @@ use Doctrine\ORM\Mapping as ORM;
 class History {
 
 	/**
+	 * @var \TYPO3\Flow\Security\Account
+	 * @ORM\OneToOne(mappedBy="accountIdentifier")
+	 */
+	protected $issuer;
+
+	/**
 	 * @var string
 	 */
-	protected $name;
+	protected $entityIdentifier;
 
+	/**
+	 * @var \string
+	 */
+	protected $entityType;
+
+	/**
+	 * @var \string
+	 */
+	protected $previousData;
+
+	/**
+	 * @var \DateTime
+	 */
+	protected $creationDate;
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getCreationDate()
+	{
+		return $this->creationDate;
+	}
 
 	/**
 	 * @return string
 	 */
-	public function getName() {
-		return $this->name;
+	public function getEntityIdentifier()
+	{
+		return $this->entityIdentifier;
 	}
 
 	/**
-	 * @param string $name
-	 * @return void
+	 * @return string
 	 */
-	public function setName($name) {
-		$this->name = $name;
+	public function getEntityType()
+	{
+		return $this->entityType;
 	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getIssuer()
+	{
+		return $this->issuer;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPreviousData()
+	{
+		return $this->previousData;
+	}
+
+	/**
+	 * @param \DateTime $creationDate
+	 */
+	public function setCreationDate($creationDate)
+	{
+		$this->creationDate = $creationDate;
+	}
+
+	/**
+	 * @param string $entityIdentifier
+	 */
+	public function setEntityIdentifier($entityIdentifier)
+	{
+		$this->entityIdentifier = $entityIdentifier;
+	}
+
+	/**
+	 * @param string $entityType
+	 */
+	public function setEntityType($entityType)
+	{
+		$this->entityType = $entityType;
+	}
+
+	/**
+	 * @param mixed $issuer
+	 */
+	public function setIssuer($issuer)
+	{
+		$this->issuer = $issuer;
+	}
+
+	/**
+	 * @param string $previousData
+	 */
+	public function setPreviousData($previousData)
+	{
+		$this->previousData = $previousData;
+	}
+	
 
 }
