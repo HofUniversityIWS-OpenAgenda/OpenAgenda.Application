d3e1b4f9d6e9224c99ee446541a088d06fa898ef
Andreas Steiger <andreas.steiger@hof-university.de>
2014-11-25 15:49:55 +0100

    [TASK] Clean-up TestDataCommandController

--- Classes/OpenAgenda/Application/Command/TestDataCommandController.php
+++ Classes/OpenAgenda/Application/Command/TestDataCommandController.php
@@ -6,15 +6,13 @@ namespace OpenAgenda\Application\Command;
  *                                                                        *
  *                                                                        */
 
-use OpenAgenda\Application\Domain\Model\AgendaItem;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Cli\CommandController;
 use \OpenAgenda\Application\Domain\Model\Meeting;
 //use \OpenAgenda\Application\Domain\Model\AgendaItem;
-use DateTime;
 
 /**
- * @Flow\Scope("singleton")
+ * @author Andreas Steiger <andreas.steiger@hof-university.de>
  */
 class TestDataCommandController extends CommandController {
 
@@ -30,10 +28,10 @@ class TestDataCommandController extends CommandController {
 	 * This Command removes all meetings and creates a new meeting with dummy data to the DB.
 	 * !!! Without AgendaItem and ProtocolItem !!!
 	 *
-	 * @return void
+	 * @return string
 	 */
 	public function addMeetingsCommand() {
-		$dateNow = new DateTime('now',  new \DateTimeZone( 'GMT+1' ));
+		$dateNow = new \DateTime('now',  new \DateTimeZone( 'GMT+1' ));
 
 		$this->meetingRepository->removeAll();
 
@@ -42,16 +40,16 @@ class TestDataCommandController extends CommandController {
 
 		$newMeeting = new Meeting;
 		//$newMeeting->setAgendaItems();
-		$newMeeting->setEndDate(new DateTime('2014-11-12 13:00'));
-		$newMeeting->setModificationDate(new DateTime('2014-11-11 10:00'));
+		$newMeeting->setEndDate(new \DateTime('2014-11-12 13:00'));
+		$newMeeting->setModificationDate(new \DateTime('2014-11-11 10:00'));
 		//$newMeeting->setProtocolItems();
 		$newMeeting->setCreationDate($dateNow);
-		$newMeeting->setStartDate(new DateTime('2014-11-12 12:00'));
+		$newMeeting->setStartDate(new \DateTime('2014-11-12 12:00'));
 		$newMeeting->setStatus(1);
 		$newMeeting->setTitle('First Meeting');
 
 		$this->meetingRepository->add($newMeeting);
-		echo "Created a new meeting.";
+		return "Created a new meeting.";
 	}
 
 }
