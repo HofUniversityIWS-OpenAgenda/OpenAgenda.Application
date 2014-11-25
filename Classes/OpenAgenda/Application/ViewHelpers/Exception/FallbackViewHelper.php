c2c8270804bf8b43f31b2c8f2c0e38f90dcaa5b8
Oliver Hader <oliver@typo3.org>
2014-11-25 14:29:05 +0100

    [TASK] Integrate Exception.FallbackViewHelper

---
+++ Classes/OpenAgenda/Application/ViewHelpers/Exception/FallbackViewHelper.php
@@ -0,0 +1,32 @@
+<?php
+namespace OpenAgenda\Application\ViewHelpers\Exception;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "OpenAgenda.Application".*
+ *                                                                        */
+
+/**
+ * Class FallbackViewHelper
+ * @package OpenAgenda\Application\ViewHelpers\Exception
+ * @author Oliver Hader <oliver@typo3.org>
+ */
+class FallbackViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @param string|NULL $message
+	 * @return string
+	 */
+	public function render($message = NULL) {
+		try {
+			$content = $this->renderChildren();
+		} catch (\Exception $exception) {
+			if ($message === NULL) {
+				$message = $exception->getMessage();
+			}
+			$content = $message;
+		}
+
+		return $content;
+	}
+
+}
