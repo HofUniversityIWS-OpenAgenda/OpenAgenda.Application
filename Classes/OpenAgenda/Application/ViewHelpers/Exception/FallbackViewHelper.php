e820e79a563f79720b0fb596ef349f7ee4cc54e8
Oliver Hader <oliver@typo3.org>
2014-11-30 21:16:25 +0100

    [TASK] Add Flow annotation statements per default

--- Classes/OpenAgenda/Application/ViewHelpers/Exception/FallbackViewHelper.php
+++ Classes/OpenAgenda/Application/ViewHelpers/Exception/FallbackViewHelper.php
@@ -5,6 +5,8 @@ namespace OpenAgenda\Application\ViewHelpers\Exception;
  * This script belongs to the TYPO3 Flow package "OpenAgenda.Application".*
  *                                                                        */
 
+use TYPO3\Flow\Annotations as Flow;
+
 /**
  * Class FallbackViewHelper
  * @package OpenAgenda\Application\ViewHelpers\Exception
--- Classes/OpenAgenda/Application/ViewHelpers/Link/HashedViewHelper.php
+++ Classes/OpenAgenda/Application/ViewHelpers/Link/HashedViewHelper.php
@@ -5,6 +5,8 @@ namespace OpenAgenda\Application\ViewHelpers\Link;
  * This script belongs to the TYPO3 Flow package "OpenAgenda.Application".*
  *                                                                        */
 
+use TYPO3\Flow\Annotations as Flow;
+
 /**
  * Class SubjectViewHelper
  *
