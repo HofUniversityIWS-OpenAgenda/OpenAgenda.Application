c18d9cd398989e6b95eb88dc733a95f016bf23a0
Oliver Hader <oliver@typo3.org>
2014-12-01 15:18:49 +0100

    [TASK] Integrate rudimentary history service
    @todo Details concerning changed properties are missing and shall be stored as serialized array in the accordant history object

---
+++ Classes/OpenAgenda/Application/Service/HistoryService.php
@@ -0,0 +1,52 @@
+<?php
+namespace OpenAgenda\Application\Service;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "OpenAgenda.Application".*
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use OpenAgenda\Application\Domain\Model\History;
+
+/**
+ * Class HistoryService
+ *
+ * @Flow\Scope("singleton")
+ * @package OpenAgenda\Application\Service
+ */
+class HistoryService {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Security\Context
+	 */
+	protected $securityContext;
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Persistence\PersistenceManagerInterface
+	 */
+	protected $persistenceManager;
+
+	/**
+	 * @Flow\Inject
+	 * @var \OpenAgenda\Application\Domain\Repository\HistoryRepository
+	 */
+	protected $historyRepository;
+
+	/**
+	 * @param object $subject
+	 * @throws \TYPO3\Flow\Persistence\Exception\IllegalObjectTypeException
+	 */
+	public function invoke($subject) {
+		$history = new History();
+		$history->setCreationDate(new \DateTime());
+		$history->setEntityType(get_class($subject));
+		$history->setIssuer($this->securityContext->getParty());
+		$history->setEntityIdentifier($this->persistenceManager->getIdentifierByObject($subject));
+		// @todo Determine changes in current modified object and persisted entity
+		$this->historyRepository->add($history);
+	}
+
+}
