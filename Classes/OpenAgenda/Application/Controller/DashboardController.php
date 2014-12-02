d9d26c99de9f648d1223b51ddf4a677de529c3cd
Oliver Hader <oliver@typo3.org>
2014-12-02 15:20:04 +0100

    [TASK] Add basic JSON response for dashboard view

--- Classes/OpenAgenda/Application/Controller/DashboardController.php
+++ Classes/OpenAgenda/Application/Controller/DashboardController.php
@@ -11,10 +11,47 @@ use TYPO3\Flow\Annotations as Flow;
 class DashboardController extends AbstractController {
 
 	/**
+	 * @var \TYPO3\Flow\Security\AccountRepository
+	 * @Flow\Inject
+	 */
+	protected $accountRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var \OpenAgenda\Application\Domain\Repository\MeetingRepository
+	 */
+	protected $meetingRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var \OpenAgenda\Application\Domain\Repository\TaskRepository
+	 */
+	protected $taskRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var \OpenAgenda\Application\Domain\Repository\MessageRepository
+	 */
+	protected $messageRepository;
+
+	/**
 	 * @return void
 	 */
 	public function indexAction() {
-		
+		$account = $this->securityContext->getAccount();
+		/** @var \TYPO3\Party\Domain\Model\Person $person */
+		$person = $account->getParty();
+
+		$value = array(
+			'person' => array(
+				'name' => $this->arrayService->flatten($person->getName()),
+				'mail' => $person->getPrimaryElectronicAddress()->getIdentifier(),
+			),
+			'meetings' => $this->arrayService->flatten($this->meetingRepository->findAllowed(), 'list'),
+			'tasks' => $this->arrayService->flatten($this->taskRepository->findAllowed(), 'list'),
+		);
+
+		$this->view->assign('value', $value);
 	}
 
 }
