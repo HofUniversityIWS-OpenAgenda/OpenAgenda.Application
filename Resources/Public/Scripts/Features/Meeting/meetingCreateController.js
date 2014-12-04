6be50494c12e720489eea9ad4aeb1ebfe100d39f
iThomas159 <thomas_winkler@me.com>
2014-12-04 12:21:50 +0100

    [TASK] New Meeting

--- Resources/Public/Scripts/Features/Meeting/meetingCreateController.js
+++ Resources/Public/Scripts/Features/Meeting/meetingCreateController.js
@@ -9,7 +9,50 @@ angular.module("Meeting")
     function ($scope, $rootScope, $http, breadcrumbs, Meetinglist) {
         $scope.breadcrumbs = breadcrumbs;
         console.log("Create meeting Conroller loaded");
-        $scope.meeting;
+
+        function agendaItem(sorting) {
+            this.__identity = "38fa3590-9095-c080-da99-c15f1710cfed";
+            this.title;
+            this.description;
+            this.creationDate;
+            this.modificationDate;
+            this.sorting = sorting;
+            this.resources = [];
+        };
+
+        function meeting() {
+            this.__identity = "66d16457-2ebf-9a70-4368-dc73a0fd9edb";
+            this.creationDate = new Date();
+            this.endDate = null;
+            this.modificationDate = new Date();
+            this.scheduledStartDate = new Date();
+            this.startDate = "'2015-01-05T12:00:00+01:00'";
+            this.status = 0;
+            this.title = null;
+            this.place = null;
+            this.agendaItems = [new agendaItem(1)];
+            this.invitations = [];
+        };
+
+        function invitation(mail) {
+            this.id = "USERID";
+            this.mail = mail;
+        };
+
+
+        $scope.meeting = new meeting();
+
+        $scope.addNewAgendaItem = function () {
+            $scope.meeting.agendaItems.push(new agendaItem($scope.meeting.agendaItems.length + 1));
+        };
+
+        $scope.addNewInvitation = function (mail) {
+            $scope.meeting.invitations.push(new invitation(mail))
+
+        };
+        $scope.deleteInvitation = function ( idx ) {
+            $scope.meeting.invitations.splice(idx, 1);
+        };
 
         $scope.$watchCollection('meeting', function(newValue, oldValue) {
             console.log(newValue);
