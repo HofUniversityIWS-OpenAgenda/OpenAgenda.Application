a7b35ca12e0c739275577533d1b66dfecb256dad
iThomas159 <thomas_winkler@me.com>
2014-12-16 17:05:20 +0100

    [TASK] Tried to save a Task, sending to Server is successful, but the task is not saved

--- Resources/Public/Scripts/Features/Task/taskEditController.js
+++ Resources/Public/Scripts/Features/Task/taskEditController.js
@@ -1,10 +1,14 @@
 /**
- * Created by Thomas on 08.12.14.
+ * This Module contains the  possibilities to edit a task
+ *
+ * @author Thomas Winkler <thomas.winkler@hof-university.de>
  */
 
+
 angular.module("Task")
-    .controller('TaskEditCtrl', ['$scope', '$rootScope', '$resource', "TaskResourceHelper", "CommonHelperMethods", '$modal', '$log',
-        function ($scope, $rootScope, $resource, TaskResourceHelper, CommonHelperMethods, $modal, $log) {
+    /*TaskEditController is used to edit and save tasks*/
+    .controller('TaskEditCtrl', ['$scope', '$rootScope', '$http', "TaskResourceHelper", "CommonHelperMethods", '$modal', '$log',
+        function ($scope, $rootScope, $http, TaskResourceHelper, CommonHelperMethods, $modal, $log) {
             console.log("Task Edit Controller Loaded", $scope.task);
 
             $scope.open = function (size, identity) {
@@ -20,19 +24,29 @@ angular.module("Task")
                     }
                 });
 
-                modalInstance.close = function (string) {
-                    //Speichern, dann neu Laden
-                    $scope.$parent.initTable();
-                    console.log(string);
+                modalInstance.close = function (string, task) {
+
+                    //Hier ist der Standard speicher versuch
+                    //Es heist zwar das es erfolgreich gespcihert wurde, aber es wird nicht gespeichert
+
+                    $http.post('/task/'+ task.__identity +'/update.json', task).
+                        success(function(data, status, headers, config) {
+                            console.log("SUCCESSFULLY SAVED TASK");
+                            $scope.$parent.initTable();
+                        }).
+                        error(function(data, status, headers, config) {
+                            // called asynchronously if an error occurs
+                            // or server returns response with an error status.
+                        });
                     modalInstance.dismiss();
                 };
             };
         }])
+    /*This controller is used to handle the modal view to view and change a tasks state*/
     .controller('ModalInstanceCtrl', function ($scope, $modalInstance, TaskResourceHelper, CommonHelperMethods, identity) {
         TaskResourceHelper.getTaskDetail(identity).get(function (task) {
             task.dueDate = CommonHelperMethods.getDateFromJSONString(task.dueDate);
             $scope.task = task;
-            console.log('modalInstanceCtrl', task);
         });
 
         $scope.ok = function () {
@@ -40,7 +54,7 @@ angular.module("Task")
         };
 
         $scope.cancel = function () {
-            $modalInstance.dismiss('DISSMISS');
+            $modalInstance.dismiss('DISMISS');
         };
 
     });
