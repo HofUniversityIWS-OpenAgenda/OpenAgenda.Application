fafc2e0238a67132a65b244caf77b8a9439c7b6c
Oliver Hader <oliver@typo3.org>
2014-12-20 19:39:47 +0100

    [TASK] Implement user profile controller

--- Resources/Public/Scripts/Features/Setting/userProfileController.js
+++ Resources/Public/Scripts/Features/Setting/userProfileController.js
@@ -3,8 +3,38 @@
  */
 
 angular.module("Setting", [])
-    .controller('userProfileCtrl', ['$scope', '$http', '$rootScope', '$routeParams', '$resource', "breadcrumbs",
-        function($scope, $http, $rootScope, $routeParams, $resource, breadcrumbs){
+    .controller('userProfileCtrl', ['$scope', '$http', '$rootScope', '$routeParams', 'OpenAgenda.Data.Utility', 'ModalDialog', '$resource', "breadcrumbs",
+        function($scope, $http, $rootScope, $routeParams, oaUtility, ModalDialog, $resource, breadcrumbs){
             $scope.breadcrumbs = breadcrumbs;
+            $scope.profile = {};
+            $scope.password = null;
+            $scope.passwordRepeat = null;
 
+            $http.get('setting/getProfile.json').success(function(profile) {
+                $scope.profile = profile;
+            });
+
+            $scope.persist = function() {
+                $http.post('setting/updateProfile.json', { person: oaUtility.jsonCast($scope.profile) })
+                    .success(function() {
+                        var modalOptions = {
+                            headerText: 'Erfolg',
+                            bodyText: 'Das Meeting wurde erfolgreich erstellt!'
+                        };
+                        var modalDefaults = {
+                            templateUrl: '/template/modaldialog/success.html'
+                        };
+                        ModalDialog.showModal(modalDefaults, modalOptions);
+                    })
+                    .error(function() {
+                        var modalOptions = {
+                            headerText: 'Fehler',
+                            bodyText: 'Es ist ein Fehler beim Übermitteln der Daten aufgetreten! Versuchen Sie es erneut!'
+                        };
+                        var modalDefaults = {
+                            templateUrl: '/template/modaldialog/error.html'
+                        };
+                        ModalDialog.showModal(modalDefaults, modalOptions);
+                    });
+            };
 }]);
