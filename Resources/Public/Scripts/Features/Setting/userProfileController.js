61cd08d65bbecdb7878c82b9732c38645ba32657
Oliver Hader <oliver@typo3.org>
2014-12-20 19:43:52 +0100

    [TASK] Update authorship on user profile controller

--- Resources/Public/Scripts/Features/Setting/userProfileController.js
+++ Resources/Public/Scripts/Features/Setting/userProfileController.js
@@ -1,40 +1,41 @@
 /**
- * Created by Andi on 16.12.14.
+ * User Profile controller
+ * @author Oliver Hader <oliver@typo3.org>
  */
-
 angular.module("Setting", [])
-    .controller('userProfileCtrl', ['$scope', '$http', '$rootScope', '$routeParams', 'OpenAgenda.Data.Utility', 'ModalDialog', '$resource', "breadcrumbs",
-        function($scope, $http, $rootScope, $routeParams, oaUtility, ModalDialog, $resource, breadcrumbs){
-            $scope.breadcrumbs = breadcrumbs;
-            $scope.profile = {};
-            $scope.password = null;
-            $scope.passwordRepeat = null;
+.controller('userProfileCtrl', ['$scope', '$http', 'OpenAgenda.Data.Utility', 'ModalDialog', 'breadcrumbs',
+    function($scope, $http, oaUtility, ModalDialog, breadcrumbs){
+        $scope.breadcrumbs = breadcrumbs;
+        $scope.profile = {};
+        // @todo Password change is missing
+        $scope.password = null;
+        $scope.passwordRepeat = null;
 
-            $http.get('setting/getProfile.json').success(function(profile) {
-                $scope.profile = profile;
-            });
+        $http.get('setting/getProfile.json').success(function(profile) {
+            $scope.profile = profile;
+        });
 
-            $scope.persist = function() {
-                $http.post('setting/updateProfile.json', { person: oaUtility.jsonCast($scope.profile) })
-                    .success(function() {
-                        var modalOptions = {
-                            headerText: 'Erfolg',
-                            bodyText: 'Das Meeting wurde erfolgreich erstellt!'
-                        };
-                        var modalDefaults = {
-                            templateUrl: '/template/modaldialog/success.html'
-                        };
-                        ModalDialog.showModal(modalDefaults, modalOptions);
-                    })
-                    .error(function() {
-                        var modalOptions = {
-                            headerText: 'Fehler',
-                            bodyText: 'Es ist ein Fehler beim Übermitteln der Daten aufgetreten! Versuchen Sie es erneut!'
-                        };
-                        var modalDefaults = {
-                            templateUrl: '/template/modaldialog/error.html'
-                        };
-                        ModalDialog.showModal(modalDefaults, modalOptions);
-                    });
-            };
-}]);
+        $scope.persist = function() {
+            $http.post('setting/updateProfile.json', { person: oaUtility.jsonCast($scope.profile) })
+                .success(function() {
+                    var modalOptions = {
+                        headerText: 'Erfolg',
+                        bodyText: 'Das Meeting wurde erfolgreich erstellt!'
+                    };
+                    var modalDefaults = {
+                        templateUrl: '/template/modaldialog/success.html'
+                    };
+                    ModalDialog.showModal(modalDefaults, modalOptions);
+                })
+                .error(function() {
+                    var modalOptions = {
+                        headerText: 'Fehler',
+                        bodyText: 'Es ist ein Fehler beim Übermitteln der Daten aufgetreten! Versuchen Sie es erneut!'
+                    };
+                    var modalDefaults = {
+                        templateUrl: '/template/modaldialog/error.html'
+                    };
+                    ModalDialog.showModal(modalDefaults, modalOptions);
+                });
+        };
+    }]);
