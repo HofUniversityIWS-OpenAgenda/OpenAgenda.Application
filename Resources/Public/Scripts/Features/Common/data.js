f4a4e0d0ebbafc892b0b4e4de797d0a7bbe126e4
Oliver Hader <oliver@typo3.org>
2015-01-12 12:03:54 +0100

    [TASK] Extend JSdoc comments

--- Resources/Public/Scripts/Features/Common/data.js
+++ Resources/Public/Scripts/Features/Common/data.js
@@ -1,8 +1,24 @@
 /**
- * @module Data
+ * @namespace angular_module.OpenAgenda
+ * @memberOf angular_module
+ */
+
+/**
+ * @deprecated The OpenAgenda.Data module is experimental and currently not used at all!
  *
- * @description General data handling and interaction methods
+ * @description Data transfer layer and HTTP interception methods.
  *
+ * <p>
+ * This module keeps track of failed HTTP requests using the accordant HTTP interceptor
+ * for each scope. Scopes in this regard are "fetchAll", "fetch", "save" and "remove", thus
+ * the typical CRUD actions. The data stores are temporarily persisted in the browser's
+ * local storage.
+ * </p>
+ *
+ * @example OpenAgenda.Data.MeetingStore.fetchAll().then(function(data) { });
+ *
+ * @class Data
+ * @memberOf angular_module.OpenAgenda
  * @author Oliver Hader <oliver@typo3.org>
  */
 angular.module('OpenAgenda.Data', [])
@@ -31,6 +47,14 @@ angular.module('OpenAgenda.Data', [])
 		}
 	}
 }])
+/**
+ * @deprecated The OpenAgenda.Data module is experimental and currently not used at all!
+ *
+ * @class Interceptor
+ * @memberOf angular_module.OpenAgenda.Data
+ * @description HTTP interception handler calling a callback function in the concrete Data.Store instance
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('OpenAgenda.Data.Interceptor', [function() {
 	return {
 		response: function (response) {
@@ -41,10 +65,24 @@ angular.module('OpenAgenda.Data', [])
 		}
 	};
 }])
+/**
+ * @deprecated The OpenAgenda.Data module is experimental and currently not used at all!
+ *
+ * @abstract
+ * @class Store
+ * @memberOf angular_module.OpenAgenda.Data
+ * @description Abstract data store defining various data-handling methods
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('OpenAgenda.Data.Store', ['$q', '$resource', 'OpenAgenda.Data.Utility', 'OpenAgenda.Data.LocalStorage', 'OpenAgenda.Data.Interceptor',
 	function($q, $resource, oaUtility, oaLocalStorage, oaInterceptor) {
 	var stores = {};
 
+	/**
+	 * @constructor
+	 * @memberOf angular_module.OpenAgenda.Data
+	 * @param {string} storeName Name/type of the store (e.g. "Meeting" or "Task")
+	 */
 	function Store(storeName) {
 		var name = storeName;
 		var identifier = '__identitity';
@@ -64,6 +102,13 @@ angular.module('OpenAgenda.Data', [])
 
 		var subjects = {};
 
+		/**
+		 * @private
+		 * @method getInterceptor
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object} subject
+		 * @return {string}
+		 */
 		function getIdentifier(subject) {
 			if (typeof subject === 'object' && typeof subject[identifier] === 'string') {
 				return subject[identifier];
@@ -73,23 +118,52 @@ angular.module('OpenAgenda.Data', [])
 			return null;
 		}
 
+		/**
+		 * @private
+		 * @method getInterceptor
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object} data
+		 */
 		function getInterceptor(data) {
 			subjects[getIdentifier(data)] = data;
 			console.log(data);
 		}
 
+		/**
+		 * @private
+		 * @method saveInterceptor
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object} data
+		 */
 		function saveInterceptor(data) {
 
 		}
 
+		/**
+		 * @private
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object} data
+		 */
 		function queryInterceptor(data) {
 
 		}
 
+		/**
+		 * @private
+		 * @method removeInterceptor
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object} data
+		 */
 		function removeInterceptor(data) {
 
 		}
 
+		/**
+		 * @private
+		 * @method access
+		 * @param {object} resourceResult
+		 * @returns {{access: Function, invoke: Function}}
+		 */
 		function access(resourceResult) {
 			return {
 				access: function() {
@@ -101,18 +175,44 @@ angular.module('OpenAgenda.Data', [])
 			}
 		}
 
+		/**
+		 * @private
+		 * @method accessWrap
+		 * @param {object} deferred
+		 * @returns {Function}
+		 */
 		function accessWrap(deferred) {
 			return function(resourceResult) {
 				deferred.resolve(resourceResult);
 			}
 		}
 
+		/**
+		 * @description Fetches all objects
+		 * @method fetchAll
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @returns {angular_module.$resource}
+		 */
 		this.fetchAll = function() {
 			return $resource(endpoints.fetchAll, { name: name }, defaultActions);
 		};
+		/**
+		 * @description Fetches one object
+		 * @method fetch
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object|string} subject
+		 * @returns {angular_module.$resource}
+		 */
 		this.fetch = function(subject) {
 			return $resource(endpoints.fetch, { name: name, identifier: getIdentifier(subject) }, defaultActions);
 		};
+		/**
+		 * @description Saves one object
+		 * @method save
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object|string} subject
+		 * @returns {angular_module.$resource}
+		 */
 		this.save = function(subject) {
 			if (getIdentifier(subject) === null) {
 				return $resource(endpoints.create, { name: name, identifier: getIdentifier(subject) }, defaultActions);
@@ -120,6 +220,13 @@ angular.module('OpenAgenda.Data', [])
 				return $resource(endpoints.update, { name: name, identifier: getIdentifier(subject) }, defaultActions);
 			}
 		};
+		/**
+		 * @decription Removes one object
+		 * @method remove
+		 * @memberOf angular_module.OpenAgenda.Data.Store
+		 * @param {object|string} subject
+		 * @returns {angular_module.$resource}
+		 */
 		this.remove = function(subject) {
 			return $resource(endpoints.remove, { name: name, identifier: getIdentifier(subject) }, defaultActions);
 		}
@@ -135,21 +242,60 @@ angular.module('OpenAgenda.Data', [])
 		}
 	}
 }])
+/**
+ * @deprecated The OpenAgenda.Data module is experimental and currently not used at all!
+ *
+ * @class MeetingStore
+ * @memberOf angular_module.OpenAgenda.Data
+ * @description Concrete instance of a data store for Meeting objects
+ * @param {object} OpenAgenda.Data.Store The abstract data store
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('OpenAgenda.Data.MeetingStore', ['OpenAgenda.Data.Store', function(oaDataStore) {
 	return oaDataStore.get('meeting');
 }])
+/**
+ * @deprecated The OpenAgenda.Data module is experimental and currently not used at all!
+ *
+ * @class TaskStore
+ * @memberOf angular_module.OpenAgenda.Data
+ * @description Concrete instance of a data store for Task objects
+ * @param {object} OpenAgenda.Data.Store The abstract data store
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('OpenAgenda.Data.TaskStore', ['OpenAgenda.Data.Store', function(oaDataStore) {
 	return oaDataStore.get('task');
 }])
+/**
+ * @deprecated The OpenAgenda.Data module is experimental and currently not used at all!
+ *
+ * @class LocalStorage
+ * @memberOf angular_module.OpenAgenda.Data
+ * @description Wrapper for browser's local storage implementation
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('OpenAgenda.Data.LocalStorage', ['$window', '$rootScope', function($window, $rootScope) {
 	angular.element($window).on('storage', function(event) {
 		console.log(event);
 	});
 
 	return {
+		/**
+		 * @description Sets values in local storage
+		 * @method set
+		 * @memberOf angular_module.OpenAgenda.Data.LocalStorage
+		 * @param {string} key
+		 * @param {mixed} value
+		 */
 		set: function(key, value) {
 			$window.localStorage.setItem('OpenAgenda.Data.LocalStorage::' + key, value);
 		},
+		/**
+		 * @description Gets values from local storage
+		 * @method get
+		 * @memberOf angular_module.OpenAgenda.Data.LocalStorage
+		 * @param {string} key
+		 */
 		get: function(key) {
 			return $window.localStorage.getItem('OpenAgenda.Data.LocalStorage::' + key);
 		}
--- Resources/Public/Scripts/Features/Common/http.js
+++ Resources/Public/Scripts/Features/Common/http.js
@@ -1,15 +1,21 @@
 /**
- * @module Http
+ * @deprecated The Http module is experimental and currently not used at all!
  *
+ * @class angular_module.Http
+ * @memberOf angular_module
  * @description Http module for capturing requests
  *
+ * <p>
  * Use the configuration property *proxy* to enable this feature.
  * The HttpInterceptor takes care of failed requests.
  * The HttpManager interacts with the HttpQueue to replay requests.
  * The HttpHeartbeat instance checks the Navigator and Server status.
+ * </p>
  *
+ * <p>
  * Changes to the online status are issued using the $rootScope.
  * $rootScope.$watch('online', function(newValue, oldValue) { ... });
+ * </p>
  *
  * @example
  * $http.get('url', {}, { proxy: true });
@@ -18,9 +24,24 @@
  * @author Oliver Hader <oliver@typo3.org>
  */
 angular.module('Http', [])
+/**
+ * @description Constructor to instantiate the heartbeat service
+ * @method run
+ * @memberOf angular_module.Http
+ * @param {object} $rootScope {@link http://docs.angularjs.org/api/ng.$rootScope}
+ * @param {object} HttpManager Http.HttpManager of this module
+ * @param {object} HttpHeartbeat Http.HttpHeartbeat of this module
+ */
 .run(['$rootScope', 'HttpManager', 'HttpHeartbeat', function($rootScope, HttpManager, HttpHeartbeat) {
 	$rootScope.$watch('online', handleOnline);
 
+	/**
+	 * @description Handles the status change triggered by the heartbeat service
+	 * @function handleOnline
+	 * @memberOf angular_module.Http
+	 * @param {bool} newValue New status value
+	 * @param {bool} oldValue Previous status value
+	 */
 	function handleOnline(newValue, oldValue) {
 		if (typeof newValue === 'undefined' || typeof oldValue === 'undefined' || newValue === oldValue) {
 			return;
@@ -32,9 +53,26 @@ angular.module('Http', [])
 
 	HttpHeartbeat.start();
 }])
+/**
+ * @description Constructor configuration injection
+ * @method config
+ * @memberOf angular_module.Http
+ * @param {object} $httpProvider {@link http://docs.angularjs.org/api/ng.$httpProvider}
+ */
 .config(['$httpProvider', function($httpProvider) {
 	$httpProvider.interceptors.push('HttpInterceptor');
 }])
+/**
+ * @deprecated The Http module is experimental and currently not used at all!
+ *
+ * @description HttpManager factory to replay failed requests
+ * @class HttpManager
+ * @memberOf angular_module.Http
+ * @param {object} $rootScope {@link http://docs.angularjs.org/api/ng.$rootScope}
+ * @param {object} $http {@link http://docs.angularjs.org/api/ng.$http}
+ * @param {object} HttpQueue Http.HttpQueue of this module
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('HttpManager', ['$rootScope', '$http', 'HttpQueue', function($rootScope, $http, HttpQueue) {
 	function replay(request, proxyId) {
 		request.config.proxyReplay++;
@@ -47,6 +85,14 @@ angular.module('Http', [])
 		}
 	}
 }])
+/**
+ * @deprecated The Http module is experimental and currently not used at all!
+ *
+ * @description HttpQueue factory to enqueue requests in general
+ * @class HttpQueue
+ * @memberOf angular_module.Http
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('HttpQueue', function() {
 	var requests = {};
 
@@ -96,6 +142,18 @@ angular.module('Http', [])
 		}
 	}
 })
+/**
+ * @deprecated The Http module is experimental and currently not used at all!
+ *
+ * @description HttpHeartbeat factory to keep track of browser connectivity and remote server avilability
+ * @class HttpHeartbeat
+ * @memberOf angular_module.Http
+ * @param {object} $window {@link http://docs.angularjs.org/api/ng.$window}
+ * @param {object} $rootScope {@link http://docs.angularjs.org/api/ng.$rootScope}
+ * @param {object} $http {@link http://docs.angularjs.org/api/ng.$http}
+ * @param {object} $interval {@link http://docs.angularjs.org/api/ng.$interval}
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('HttpHeartbeat', ['$window', '$rootScope', '$http', '$interval', function($window, $rootScope, $http, $interval) {
 	var interval;
 	var serverOnline;
@@ -135,6 +193,17 @@ angular.module('Http', [])
 		}
 	};
 }])
+/**
+ * @deprecated The Http module is experimental and currently not used at all!
+ *
+ * @description HttpInterceptor factory to keep track of successful and failed HTTP request
+ * @class HttpInterceptor
+ * @memberOf angular_module.Http
+ * @param {object} $q {@link http://docs.angularjs.org/api/ng.$q}
+ * @param {object} $location {@link http://docs.angularjs.org/api/ng.$location}
+ * @param {object} HttpQueue Http.HttpQueue of this module
+ * @author Oliver Hader <oliver@typo3.org>
+ */
 .factory('HttpInterceptor', ['$q', '$location', 'HttpQueue', function($q, $location, HttpQueue) {
 	return {
 		request: function(config) {
